b2b77a1338812248e598d2e2b00f8fadc2fbf970
David Yin <29613693+E137299@users.noreply.github.com>
2025-09-25 09:44:54 -0500

    Finished?

--- Display.java
+++ Display.java
@@ -3,13 +3,13 @@ import java.awt.*;
 import java.util.ArrayList;
 
 
-public class Display extends JPanel{
+public class Display extends JPanel
+{
 	public Display(){}
-	// paintCommponent is not explicitly called. It is executed whenever the JPanel is called.
-	// Graphics g object is implicitly passed to function.
-	public void paintComponent(Graphics g){
-		super.paintComponent(g); //satisfies the parent class' requirements.
-		setBackground(Color.WHITE);
 
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		setBackground(Color.BLACK);
 	}
 }
