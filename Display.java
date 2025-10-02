4d7600b42ffc8460318ff34091b9495e4a1c0ad6
SalvadorTangaroa <148146165+SalvadorTangaroa@users.noreply.github.com>
2025-10-02 10:02:07 -0500

    Update Display.java

--- Display.java
+++ Display.java
@@ -11,5 +11,17 @@ public class Display extends JPanel
 	{
 		super.paintComponent(g);
 		setBackground(Color.BLACK);
+
+		g.setColor(Color.RED);
+        g.fillRect(50, 50, 150, 100);
+
+        g.setColor(Color.BLUE);
+        g.fillRect(250, 50, 100, 200);
+
+        g.setColor(Color.GREEN);
+        g.fillOval(100, 250, 100, 100);
+
+        g.setColor(Color.YELLOW);
+        g.fillOval(300, 300, 150, 150);
 	}
 }
