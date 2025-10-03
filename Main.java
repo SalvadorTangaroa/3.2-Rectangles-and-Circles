6c9cef9e8278c758c50fb9b0f06fa2c4fbabd172
SalvadorTangaroa <148146165+SalvadorTangaroa@users.noreply.github.com>
2025-10-03 09:08:26 -0500

    finished

--- Main.java
+++ Main.java
@@ -1,15 +1,34 @@
-import javax.swing.*;
-import java.awt.*;
+public class Rectangle {
+    private double myX;
+    private double myY;
+    private double myWidth;
+    private double myHeight;
 
-public class Main
-{
-	public static void main(String[] args)
-	{
-		JFrame frame = new JFrame("Drawing");
-		frame.setSize(600,600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
-		frame.add(new Display());
-		frame.setVisible(true);
-	}
+    // Default constructor
+    public Rectangle() {
+        myX = 0;
+        myY = 0;
+        myWidth = 0;
+        myHeight = 0;
+    }
+
+    // Parameterized constructor
+    public Rectangle(double x, double y, double width, double height) {
+        myX = x;
+        myY = y;
+        myWidth = width;
+        myHeight = height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (myWidth + myHeight);
+    }
+
+    public double getArea() {
+        return myWidth * myHeight;
+    }
+
+    Rectangle rectA = new Rectangle();
+    Rectangle rectB = new Rectangle(0, -80, 400, 160);
+    Rectangle rectC = new Rectangle(100, -100, 20, 300);
 }
